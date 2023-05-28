ff273a3699cea8ad33729acda3597f5b63c28b66
biangomes <beanascigom@gmail.com>
2023-05-28 12:35:24 -0300

    Method save created in the CarServiceTest()!

--- VehiclesRent/src/test/java/com/example/vehiclesrent/services/CarServiceTest.java
+++ VehiclesRent/src/test/java/com/example/vehiclesrent/services/CarServiceTest.java
@@ -2,13 +2,24 @@ package com.example.vehiclesrent.services;
 
 import com.example.vehiclesrent.model.Car;
 import com.example.vehiclesrent.repository.CarRepository;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -19,15 +30,40 @@ public class CarServiceTest {
 
     @Mock
     private CarRepository carRepository;
-    private Car car;
+    
+    @InjectMocks
+    private CarService service;
+    
+    // private Car car;
 
-    @BeforeEach
-    public void setup() {
+    // @BeforeEach
+    // public void setup() {
+
+    // }
+
+
+    @DisplayName("Must return a saved car")
+    @Test
+    public void must_save_one_car() {
+        
+        Car car = new Car();
         car.setUuid(UUID.randomUUID());
         car.setModel("Civic 2009");
         car.setBrand("Honda");
         car.setCreatedAt(LocalDateTime.now());
-    }
 
+
+        when(carRepository.save(car)).thenReturn(car);
+
+        Car savedCar = service.save(car);
+
+        assertNotNull(savedCar);
+        assertEquals(car.getModel(), savedCar.getModel());
+        assertEquals(car.getBrand(), savedCar.getBrand());
+        assertEquals(car.getUuid(), savedCar.getUuid());
+
+
+        verify(carRepository, times(1)).save(car);
+    }
 
 }
