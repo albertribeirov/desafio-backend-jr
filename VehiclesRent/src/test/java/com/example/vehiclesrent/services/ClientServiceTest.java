722d38b9edff8fff701e5056435f44b8e02f7d32
biangomes <beanascigom@gmail.com>
2023-05-30 17:23:15 -0300

    test must return one car by ID created and passed

--- VehiclesRent/src/test/java/com/example/vehiclesrent/services/ClientServiceTest.java
+++ VehiclesRent/src/test/java/com/example/vehiclesrent/services/ClientServiceTest.java
@@ -1,13 +1,25 @@
 package com.example.vehiclesrent.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.example.vehiclesrent.model.Car;
 import com.example.vehiclesrent.model.Client;
 import com.example.vehiclesrent.repository.ClientRepository;
 
@@ -20,8 +32,27 @@ public class ClientServiceTest {
     @InjectMocks
     private ClientService service;
 
+
     private final List<Client> clients = new ArrayList<>();
 
+    Car car = new Car(UUID.randomUUID(), "Aston Martin", "GPX", LocalDateTime.now());
 
-    
+    @DisplayName("Must return one car by ID")
+    @Test
+    public void must_return_one_car_by_id() {
+        UUID exampleId = UUID.randomUUID();
+
+        Client client1 = new Client(exampleId, "Kane", "300.300.300-30", car, LocalDate.of(1998, Month.JULY, 31), LocalDateTime.now(), LocalDateTime.now());
+        repo.save(client1);
+
+        Mockito.when(repo.findById(exampleId)).thenReturn(Optional.of(client1));
+
+        Client savedClient = repo.findById(client1.getUuid()).orElse(null);
+
+        assertNotNull(savedClient);
+        assertEquals(savedClient.getUuid(), client1.getUuid());
+        assertEquals(savedClient.getCpf(), client1.getCpf());
+        assertEquals(savedClient.getName(), savedClient.getName());
+
+    }
 }
